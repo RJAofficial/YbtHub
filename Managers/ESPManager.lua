012d8bc9fa7bac1f08f8c2b9966601691dfb92b7
RJA <rasya.justicio.official@gmail.com>
2022-06-25 23:04:52 +0700

    Update (0.0.2)

--- Managers/ESPManager.lua
+++ Managers/ESPManager.lua
@@ -26,3 +26,65 @@ end
 
 
 -- Module:
+local ESPManager = {
+    ESP = {
+        Enabled = true,
+        EnabledBind = "M",
+        TeamColor = false,
+        TeamCheck = false,
+
+        Boxes = {
+            Enabled = true,
+            Color3 = Color3.fromRGB(0, 0, 0),
+            OutlineColor3 = Color3.fromRGB(255, 255, 255),
+            Thickness = 2,
+            OutlineThickness = 4,
+            FaceCamera = false,
+
+            Objects = setmetatable({}, {__mode = "kv"})
+        },
+    }
+}
+
+function ESPManager:getTeam(player)
+    if player and player.Team then
+        return player.Team
+    end
+end
+
+function ESPManager:isTeammate(player)
+    if player and (self:getTeam(player) == self:getTeam(LocalPlayer)) then
+        return true
+    else 
+        return false
+    end
+end
+
+function ESPManager:getPlayerFromCharacter(character)
+    if character then
+        return LocalPlayer:GetPlayerFromCharacter(character)
+    end
+end
+
+function ESPManager:getColor(character, types)
+    if character then
+        local player = self:getPlayerFromCharacter(character)
+        if player then
+            if types == "Box" then
+                if player.Team and self.ESP.TeamColor == true then
+                    return player.Team.TeamColor.Color
+                else
+                    return self.ESP.Boxes.Color3, self.ESP.Boxes.OutlineColor3
+                end
+            end
+        end 
+    end
+end
+
+function ESPManager:addBox(object, options)
+    
+end
+
+
+
+return ESPManager
